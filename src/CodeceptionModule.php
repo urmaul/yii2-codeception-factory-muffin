7fc37acc9ba38915a9dd2c4123c9ec3bf3fdb29a
urmaul <bogdan@itechcraft.com>
2015-03-12 16:23:05 +0200

    CodeceptionModule class

---
+++ src/CodeceptionModule.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace urmaul\yii2\FactoryMuffin;
+
+use League\FactoryMuffin\Factory;
+use League\FactoryMuffin\Facade as FactoryMuffin;
+
+// here you can define custom actions
+// all public methods declared in helper class will be available in $I
+
+class CodeceptionModule extends \Codeception\Module
+{
+    /**
+     * @var Factory FactoryMuffin instance.
+     */
+    protected $factory;
+    
+    /**
+     * @var boolean true if you want to delete saved models after test.
+     */
+    protected $doDeleteSaved = true;
+    
+    public function _initialize()
+    {
+        $this->factory = FactoryMuffin::reset();
+    }
+    
+    /**
+     * Creates and saves in db an instance of the model.
+     *
+     * This object will be generated with mock attributes.
+     *
+     * @param string $model The model class name.
+     * @param array  $attr  The model attributes.
+     *
+     * @return \yii\base\Model
+     */
+    public function create($model, $attr = array())
+    {
+        return $this->factory->create($model, $attr);
+    }
+    
+    /**
+     * Return an instance of the model.
+     *
+     * This does not save it in the database. Use create for that.
+     *
+     * @param string $model The model class name.
+     * @param array  $attr  The model attributes.
+     *
+     * @return \yii\base\Model
+     */
+    public function instance($model, $attr = array())
+    {
+        return $this->factory->instance($model, $attr);
+    }
+    
+    public function _after(\Codeception\TestCase $test)
+    {
+        if ($this->doDeleteSaved)
+            $this->factory->deleteSaved();
+    }
+}
