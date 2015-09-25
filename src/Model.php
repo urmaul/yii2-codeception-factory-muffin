1ba798e6ead4d90a9e5d0f7329f83754f22a52cd
urmaul <bogdan@itechcraft.com>
2015-09-25 15:36:15 +0300

    getValidationErrors returns null if no errors

--- src/Model.php
+++ src/Model.php
@@ -9,10 +9,13 @@ trait Model
 {
     /**
      * Getter used by FactoryMuffin in unit tests.
-     * @return sting
+     * @return string|null
      */
     public function getValidationErrors()
     {
+        if (!$this->hasErrors())
+            return null;
+        
         $result = '';
         foreach ($this->errors as $lines) {
             $result .= implode("\n", $lines) . "\n";
